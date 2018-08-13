f7e02dbb7b0920b18f267041c7768570791108fe
maryleo <jesus.antonio@mdp.com.pe>
2018-08-13 17:22:19 -0500

    boilerplate

---
+++ src/app/shared/configs.ts
@@ -0,0 +1,11 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+export const Configs = {
+  appId: '8a783b11-f9c2-42c6-9456-078f30c03b51',
+  scope: 'User.Read Mail.Send Calendars.ReadWrite Calendars.ReadWrite.Shared Bookings.ReadWrite.All'
+};
+//openid profile User.Read Mail.Read Calendars.Read Contacts.Read 
+//User.Read Mail.Send Calendars.Send Calendars.Read Calendars.ReadWrite Calendars.Events Bookings.ReadWrite.All
