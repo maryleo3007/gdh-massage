b03c89151bbaf0dac29590a99f6b68b36b13cfaf
AlejandraCP <alejandracp710@gmail.com>
2018-08-27 17:55:30 -0500

    no order

--- src/app/view-coor/view-coor.component.ts
+++ src/app/view-coor/view-coor.component.ts
@@ -20,14 +20,13 @@ export class ViewCoorComponent implements OnInit {
   public isLogin: boolean;
   public name: string;
   public correctUser: boolean;
-  public show:boolean;
+  public show: boolean;
   public actualDate: string;
   inscriptionList: any[];
   therapist1List: any[];
   therapist2List: any[];
   therapist3List: any[];
-  orderArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  turnsHours = ['12:00', '12:20', '12:40', '1:00', '1:20', '1:40', '2:00', '2:20', '2:40', '3:00', '3:20', '3:40']
+  orderArr = [{ turn: '12:00' }, { turn: '12:20' }, { turn: '12:40' }, { turn: '1:00' }, { turn: '1:20' }, { turn: '1:40' }, { turn: '2:00' }, { turn: '2:20' }, { turn: '2:40' }, { turn: '3:00' }, { turn: '3:20' }, { turn: '3:40' }]
   reportList: any[];
 
   constructor(
@@ -39,18 +38,18 @@ export class ViewCoorComponent implements OnInit {
   ngOnInit() {
     this.isLogin = false;
     this.authFirebaseService.getAuth().subscribe(auth => {
-      if(auth){
+      if (auth) {
         this.emailUser = auth.email;
         this.isLogin = true;
-        if(auth.email === 'aponcedeleon@inteligogroup.com' || auth.email === 'valvarez@inteligogroup.com' ||
+        if (auth.email === 'aponcedeleon@inteligogroup.com' || auth.email === 'valvarez@inteligogroup.com' ||
           auth.email === 'mllamocca@inteligogroup.com' || auth.email === 'acabrera@inteligogroup.com' ||
           auth.email === 'linga@inteligogroup.com' || auth.email === 'coordinadora@inteligogroup.com'
-      ) {
+        ) {
           this.show = true;
-        } else if (auth.email == undefined || auth.email == null){
-          this.show  = false;
+        } else if (auth.email == undefined || auth.email == null) {
+          this.show = false;
         }
-      }     
+      }
     });
 
     // current date
@@ -64,61 +63,71 @@ export class ViewCoorComponent implements OnInit {
     if (mm < 10) {
       mm = '0' + mm;
     }
-    this.actualDate = dd + '/' + mm + '/' + yyyy;
+    this.actualDate = 21 + '/' + mm + '/' + yyyy;
 
     // get inscriptions
     this.inscriptionService.getInscriptions()
-    .snapshotChanges()
-    .subscribe(item => {
-      this.inscriptionList = [];
-      this.therapist1List = [];
-      this.therapist2List = [];
-      this.therapist3List = [];
-      item.forEach(elem => {
-        let x = elem.payload.toJSON();
-        x["$key"] = elem.key;
-        if(x['therapist'] === 1) {
-          this.orderArr.forEach(elem => {
-            if(x['hourStart'] === this.turnsHours[0]) {
-
+      .snapshotChanges()
+      .subscribe(item => {
+        this.inscriptionList = [];
+        this.therapist1List = [];
+        this.therapist2List = [];
+        this.therapist3List = [];
+        item.forEach(elem => {
+          let x = elem.payload.toJSON();
+          x["$key"] = elem.key;
+          if (x['date'] === this.actualDate) {
+            if (x['therapist'] === 1) {
+              for (const i in this.orderArr) {
+                if (x['hourStart'] === this.orderArr[i].turn) {
+                  x['order'] = i;
+                  this.therapist1List.push(x);
+                }
+              }
+            } else if (x['therapist'] === 2) {
+              for (const i in this.orderArr) {
+                if (x['hourStart'] === this.orderArr[i].turn) {
+                  x['order'] = i;
+                  this.therapist2List.push(x);
+                }
+              }
+            } else if (x['therapist'] === 3) {
+              for (const i in this.orderArr) {
+                if (x['hourStart'] === this.orderArr[i].turn) {
+                  x['order'] = i;
+                  this.therapist3List.push(x);
+                }
+              }
             }
-          })
-          // if(x['hourStart'] === '12:00'){
-          //   x["order"] = this.orderArr[0];
-          //   // console.log(x['hourStart'])
-          //   this.therapist1List.push(x);
-          //   // console.log(this.therapist1List)
-          // } else if (x['hourStart'] === '12:20') {
-          //   x["order"] = this.orderArr[1];
-          //   // console.log(x['hourStart'])
-          //   this.therapist1List.push(x);
-          //   console.log(this.therapist1List)
-          // }
-          // console.log(x['hourStart'])
-          // console.log(this.orderArr[0])
-          // this.therapist1List.push(x);
-        } else if (x['therapist'] === 2) {
-          this.therapist2List.push(x);
-        } else if (x['therapist'] === 3) {
-          this.therapist3List.push(x);
-        }
-        this.inscriptionList.push(x);
+            this.inscriptionList.push(x);
+          }
+        });
+        console.log(this.therapist1List);
+        // console.log(this.therapist2List);
+        // console.log(this.therapist3List);
+        // console.log(date)
       });
-    });    
+
+      this.therapist1List.sort(function (o1,o2) {
+        if (o1 > o2) { //comparación lexicogŕafica
+          return 1;
+          // console.log('works')
+        } 
+      });
 
     // get reports
     this.reportService.getReports()
-    .snapshotChanges()
-    .subscribe(item => {
-      this.reportList = [];
-      item.forEach(elem => {
-        let x = elem.payload.toJSON();
-        x['$key'] = elem.key;
-        this.reportList.push(x)
+      .snapshotChanges()
+      .subscribe(item => {
+        this.reportList = [];
+        item.forEach(elem => {
+          let x = elem.payload.toJSON();
+          x['$key'] = elem.key;
+          this.reportList.push(x)
+        })
       })
-    })
 
-    
+
   }
 
   logoutUser() {
