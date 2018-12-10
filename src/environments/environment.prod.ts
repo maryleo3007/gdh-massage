39d29a2608d72c40c5b3cbd3f2a70ab7fe7dc90a
maryleo <jesus.antonio@mdp.com.pe>
2018-12-10 18:02:13 -0500

    add url firebase gdh-masajes-produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyADVjTeEvncM1LSAThpNP1dbeitVK3P6bw",
-    authDomain: "gdh-masajes-prod.firebaseapp.com",
-    databaseURL: "https://gdh-masajes-prod.firebaseio.com",
-    projectId: "gdh-masajes-prod",
-    storageBucket: "gdh-masajes-prod.appspot.com",
-    messagingSenderId: "957892310927"
+    // apiKey: "AIzaSyADVjTeEvncM1LSAThpNP1dbeitVK3P6bw",
+    // authDomain: "gdh-masajes-prod.firebaseapp.com",
+    // databaseURL: "https://gdh-masajes-prod.firebaseio.com",
+    // projectId: "gdh-masajes-prod",
+    // storageBucket: "gdh-masajes-prod.appspot.com",
+    // messagingSenderId: "957892310927"
+    
+    apiKey: "AIzaSyBm7Wdm4BYbt6QuiDBMfsYXIfI3ZC158zU",
+    authDomain: "gdh-masajes-produccion.firebaseapp.com",
+    databaseURL: "https://gdh-masajes-produccion.firebaseio.com",
+    projectId: "gdh-masajes-produccion",
+    storageBucket: "gdh-masajes-produccion.appspot.com",
+    messagingSenderId: "497018884063"
 
   }
 };
