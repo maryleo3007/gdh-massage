e246c6a978560f74b17df0db2ec6321c5aa794c1
maryleo3007 <maryleo.3007@gmail.com>
2019-04-29 20:33:16 -0500

    deploy mi bienestar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
 
     // apiKey: "AIzaSyBxdSNyiUr0zjJWKPkITsZBLulalJ-yyAQ",
@@ -8,11 +8,11 @@ export const environment = {
     // projectId: "gdh-masajes2-produccion",
     // storageBucket: "gdh-masajes2-produccion.appspot.com",
     // messagingSenderId: "498956326446"
-    apiKey: "AIzaSyBp4GPYTdYOhjYbXH8STLKnHN50YsXXSfw",
-    authDomain: "mi-bienestar-masajes.firebaseapp.com",
-    databaseURL: "https://mi-bienestar-masajes.firebaseio.com",
-    projectId: "mi-bienestar-masajes",
-    storageBucket: "mi-bienestar-masajes.appspot.com",
-    messagingSenderId: "425586986224"
+    // apiKey: "AIzaSyBp4GPYTdYOhjYbXH8STLKnHN50YsXXSfw",
+    // authDomain: "mi-bienestar-masajes.firebaseapp.com",
+    // databaseURL: "https://mi-bienestar-masajes.firebaseio.com",
+    // projectId: "mi-bienestar-masajes",
+    // storageBucket: "mi-bienestar-masajes.appspot.com",
+    // messagingSenderId: "425586986224"
   }
 };
